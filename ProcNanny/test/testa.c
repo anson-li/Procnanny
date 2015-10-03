fdd966417a6043c440a2cfe35aadca29041fb3ed
anson-li <ansonli.94@gmail.com>
2015-10-03 13:02:20 -0600

    hey

--- ProcNanny/test/testa.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-int main(int c) 
-{
-    sleep(150);
-    return 1;
-}
--- ProcNanny/test/testc.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-int main(int c) {
-    sleep(50);
-   
-    return 1;
-}
