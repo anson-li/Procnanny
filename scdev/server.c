9303e299b216c08ea75cd09a61e94d726b4eb880
Anson Li <ansonli.94@gmail.com>
2015-11-21 14:56:02 -0700

    FULL SET didn't work

--- scdev/server.c
+++ scdev/server.c
@@ -99,7 +99,7 @@ int main (int c, char *argv[])
       /* Block until input arrives on one or more active sockets. */
       read_fd_set = active_fd_set;
       write_fd_set = active_fd_set;
-      if (select (FD_SETSIZE, &read_fd_set, &write_fd_set, NULL, NULL) < 0) {
+      if (select (FD_SETSIZE, &read_fd_set, NULL, NULL, NULL) < 0) {
         perror ("select");
         exit (EXIT_FAILURE);
       }
