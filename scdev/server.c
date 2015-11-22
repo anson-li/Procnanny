d8198be71295312eb4d99aba7c78e71934875c2b
Anson Li <ansonli.94@gmail.com>
2015-11-21 17:28:39 -0700

    attempt to grab all

--- scdev/server.c
+++ scdev/server.c
@@ -59,7 +59,7 @@ read_from_client (int filedes)
   char * token;
   int nbytes;
 
-  nbytes = read (filedes, buffer, MAXMSG);
+  nbytes = read (filedes, buffer - 1, MAXMSG);
   if (nbytes < 0)
     {
       /* Read error. */
