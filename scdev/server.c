17e9d2e21b3d1cb31e4bc337d1b862e67fdcca39
Anson Li <ansonli.94@gmail.com>
2015-11-22 19:49:41 -0700

    switch to while loop for bind

--- scdev/server.c
+++ scdev/server.c
@@ -70,7 +70,7 @@ int make_socket (uint16_t port)
   name.sin_family = AF_INET;
   name.sin_port = htons (port);
   name.sin_addr.s_addr = htonl (INADDR_ANY);
-  if (bind (sock, (struct sockaddr *) &name, sizeof (name)) < 0)
+  while (bind (sock, (struct sockaddr *) &name, sizeof (name)) < 0)
     {
       perror ("bind");
       //exit (EXIT_FAILURE);
